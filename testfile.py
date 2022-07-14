c4351e7be85d0c82218626dccee380fbdb94910b
Whitepp <dana_bak@naver.com>
2022-07-15 01:13:08 +0900

    Update testfile.py

--- testfile.py
+++ testfile.py
@@ -376,7 +376,7 @@ async def on_message(message):
             return                  
 
         if content == "한줄소개설문지":
-            await message.channel.send("https://forms.gle/dtnkkugrvcT7Q1Pq8")
+            await message.channel.send("https://forms.gle/qw6SgwN6rrGmwqiT7")
             return
 
         if content == "주사위":
