41c22f4ed4795aba7024929b6ad87081c0a6c99f
Whitepp <dana_bak@naver.com>
2024-12-21 15:55:55 +0900

    Update testfile.py

--- testfile.py
+++ testfile.py
@@ -13,7 +13,7 @@ client = commands.Bot(command_prefix=">>", intents=discord.Intents.all())
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
-url = 'https://docs.google.com/spreadsheets/d/1WI7W0KjLaebqQUuLpf7BqhPYEKnLa2ppNLEiXasOce4/edit#gid=0'
+url = 'https://docs.google.com/spreadsheets/d/1uJ8H_VVMpZx454iz-sF5OUxJ0SWu4JGfRqWaaRfMKsU/edit#gid=0'
 
 current_time = lambda: datetime.datetime.utcnow() + datetime.timedelta(hours=9)
 
